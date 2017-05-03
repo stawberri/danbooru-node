a9fb3e07781d37534141424027d2bc72b80ae3c1
Pudding <aideen@aideen.pw>
2017-05-03 16:46:11 -0700

    Add rating property to posts

--- test/posts.js
+++ test/posts.js
@@ -5,12 +5,31 @@ test('post fetching', async t => {
   let booru = new Danbooru()
   t.timeoutAfter(30000)
 
-  let posts = await booru.posts('animal_ears')
-  t.true(Array.isArray(posts), 'returns an array')
-  t.true(posts.every(post => post.tags.includes('animal_ears')), 'searches for tags')
+  await Promise.all([
+    booru.posts().then(async posts => {
+      t.true(Array.isArray(posts), 'returns an array')
 
-  let post = await booru.post(posts[0].id)
-  t.deepEqual(post.raw, posts[0].raw, 'returns individual posts')
+      let post = await booru.post(posts[0].id)
+      t.equal(post.raw.id, posts[0].raw.id, 'returns individual posts')
+    }),
+    booru.posts('animal_ears').then(async posts => {
+      t.true(posts.every(post => post.tags.includes('animal_ears')), 'searches for tags')
+
+      let post = await booru.post(posts[0])
+      t.deepEqual(post.raw, posts[0].raw, 'can be used instead of ids')
+    }),
+    booru.posts(['animal ears', '1girl']).then(posts =>
+      t.true(posts.every(post => {
+        return (
+          post.tags.includes('animal_ears') &&
+          post.tags.includes('1girl')
+        )
+      }), 'supports tag arrays')
+    ),
+    booru.posts('rating:s').then(posts => {
+      t.true(posts.every(post => post.rating.s), 'has ratings')
+    })
+  ])
 
   t.end()
 })
