c76fccd74a39256c88b32cf27f7d88562fdfbe7d
Pudding <aideen@aideen.pw>
2018-02-25 03:26:27 -0800

    Rename endpoint to booru

--- test/booru.test.js
+++ test/booru.test.js
@@ -1,64 +1,64 @@
 const nock = require('nock')
-const Endpoint = require('../lib/endpoint')
+const Booru = require('../lib/booru')
 
-describe('endpoint constructor', () => {
+describe('booru constructor', () => {
   test('used by main export', () => {
     const Danbooru = require('..')
-    expect(new Danbooru()).toBeInstanceOf(Endpoint)
+    expect(new Danbooru()).toBeInstanceOf(Booru)
   })
 
   test('defaults to undefined login', () => {
-    const endpoint = new Endpoint()
-    expect(endpoint.user).toBeUndefined()
+    const booru = new Booru()
+    expect(booru.user).toBeUndefined()
   })
 
   test('has undefined login if no key is provided', () => {
-    const endpoint = new Endpoint('meow')
-    expect(endpoint.user).toBeUndefined()
+    const booru = new Booru('meow')
+    expect(booru.user).toBeUndefined()
   })
 
   test('defaults to main url', () => {
-    const endpoint = new Endpoint()
-    expect(endpoint.url).toBe('https://danbooru.donmai.us/')
+    const booru = new Booru()
+    expect(booru.url).toBe('https://danbooru.donmai.us/')
   })
 
   test('saves login', () => {
-    const endpoint = new Endpoint('login:api_key')
-    expect(endpoint).toMatchObject({
+    const booru = new Booru('login:api_key')
+    expect(booru).toMatchObject({
       user: 'login',
       url: 'https://danbooru.donmai.us/'
     })
   })
 
   test('saves login and url', () => {
-    const endpoint = new Endpoint('https://login:api_key@safebooru.donmai.us')
-    expect(endpoint).toMatchObject({
+    const booru = new Booru('https://login:api_key@safebooru.donmai.us')
+    expect(booru).toMatchObject({
       user: 'login',
       url: 'https://safebooru.donmai.us/'
     })
   })
 
   test('cleans urls', () => {
-    expect(new Endpoint('http://hijiribe.donmai.us/?query#hash').url).toBe(
+    expect(new Booru('http://hijiribe.donmai.us/?query#hash').url).toBe(
       'http://hijiribe.donmai.us/'
     )
   })
 
   test('supports paths', () => {
-    expect(new Endpoint('http://sonohara.donmai.us/path').url).toBe(
+    expect(new Booru('http://sonohara.donmai.us/path').url).toBe(
       'http://sonohara.donmai.us/path/'
     )
   })
 })
 
-describe('endpoint connectivity', () => {
+describe('booru connectivity', () => {
   test('makes requests', done => {
     const scope = nock('https://danbooru.donmai.us')
       .get('/resource')
       .reply(200, 'reply')
 
-    const endpoint = new Endpoint()
-    endpoint
+    const booru = new Booru()
+    booru
       .request({ path: '/resource' }, response => {
         expect(response.statusCode).toBe(200)
 
