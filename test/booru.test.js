925bb9d227d68ec807470cb919ff70b832aaa450
Pudding <aideen@aideen.pw>
2018-02-26 03:01:03 -0800

    Implement request methods

--- test/booru.test.js
+++ test/booru.test.js
@@ -51,7 +51,7 @@ describe('booru constructor', () => {
   })
 })
 
-describe('booru connectivity', () => {
+describe('basic requests and json', () => {
   test('basic node request', done => {
     const scope = nock('https://danbooru.donmai.us')
       .get('/a23cb13b8b18')
@@ -179,4 +179,101 @@ describe('booru connectivity', () => {
     })
     expect(scope.isDone()).toBeTruthy()
   })
+
+  test('query, body, and arrays', async () => {
+    const body = { architect: 'optimize generating Lebanese Pound' }
+    const query = ['Forge foreground Avon', 'Industrial Frozen']
+    const reply = [true, 'methodical', 34148, 'Forge Money Market Account']
+
+    const scope = nock('https://danbooru.donmai.us', {
+      reqheaders: { 'content-type': 'application/json' }
+    })
+      .put('/9b05655a2aa5.json', body)
+      .query(query)
+      .reply(200, reply)
+
+    const booru = new Booru()
+    const response = await booru.json('9b05655a2aa5', {
+      method: 'PUT',
+      body,
+      query
+    })
+
+    expect(response).toEqual(expect.arrayContaining(reply))
+    expect(response).toHaveLength(reply.length)
+    expect(scope.isDone()).toBeTruthy()
+  })
+
+  afterEach(() => nock.cleanAll())
+})
+
+describe('json request methods', () => {
+  test('get', async () => {
+    const reply = { microchip: 53142 }
+    const query = { generate: 'Direct Investment Account Tactics' }
+
+    const scope = nock('https://danbooru.donmai.us')
+      .get('/f9d4f26b44cf.json')
+      .query(query)
+      .reply(200, reply)
+
+    const booru = new Booru()
+    const response = await booru.get('f9d4f26b44cf', query)
+
+    expect(response).toMatchObject(reply)
+    expect(scope.isDone()).toBeTruthy()
+  })
+
+  test('post', async () => {
+    const body = { bypass: 'deposit', payment: ['invoice', 'connect'] }
+    const reply = { neural: 'installation Exclusive Texas' }
+
+    const scope = nock('https://danbooru.donmai.us', {
+      reqheaders: { 'content-type': 'application/json' }
+    })
+      .post('/fee2f342fbd8.json', body)
+      .reply(200, reply)
+
+    const booru = new Booru()
+    const response = await booru.post('/fee2f342fbd8', body)
+
+    expect(response).toMatchObject(reply)
+    expect(scope.isDone()).toBeTruthy()
+  })
+
+  test('put', async () => {
+    const body = { indexing: 'United States of America' }
+    const reply = { optical: 'Fall zero defect' }
+
+    const scope = nock('https://danbooru.donmai.us', {
+      reqheaders: { 'content-type': 'application/json' }
+    })
+      .put('/ff33fdc4894e.json', body)
+      .reply(200, reply)
+
+    const booru = new Booru()
+    const response = await booru.put('/ff33fdc4894e', body)
+
+    expect(response).toMatchObject(reply)
+    expect(scope.isDone()).toBeTruthy()
+  })
+
+  test('delete', async () => {
+    const body = { scale: 'Norfolk Island orange Granite' }
+    const reply = { silver: 'Wisconsin Integration cross-platform' }
+
+    const scope = nock('https://danbooru.donmai.us', {
+      reqheaders: { 'content-type': 'application/json' }
+    })
+      .delete('/77af4d0af0e9.json', body)
+      .reply(200, reply)
+
+    const booru = new Booru()
+    const response = await booru.delete('/77af4d0af0e9', body)
+
+    expect(response).toMatchObject(reply)
+    expect(scope.isDone()).toBeTruthy()
+  })
+
+  afterEach(() => nock.cleanAll())
 })
